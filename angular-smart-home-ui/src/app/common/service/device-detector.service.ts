a590a521bcfd0e61a04a61ae05e50378fb91670e
Glib Shemenkov <shemenkovglib@gmail.com>
2025-12-03 15:35:31 +0100

    feat: add device-detector.service for screen

---
+++ angular-smart-home-ui/src/app/common/service/device-detector.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { signal, Signal } from '@angular/core';
+// TODO: почитать что это inject, Injectable   providers в компонентах
+@Injectable({
+  providedIn: 'root',
+})
+export class DeviceDetectorService {
+  readonly #isMobile = signal<boolean>(false);
+  #breakpointObserver: BreakpointObserver = inject(BreakpointObserver);
+  public readonly isMobile: Signal<boolean> = this.#isMobile;
+
+  constructor() {
+    this.#breakpointObserver.observe([Breakpoints.Handset]).subscribe((result) => {
+      this.#isMobile.set(result.matches);
+    });
+  }
+}
