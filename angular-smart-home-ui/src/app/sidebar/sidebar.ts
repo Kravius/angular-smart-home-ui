c937162928da5887682774f54a22d04f583f8ccc
Glib Shemenkov <shemenkovglib@gmail.com>
2025-12-04 14:47:39 +0100

    feat: add basic design

--- angular-smart-home-ui/src/app/sidebar/sidebar.ts
+++ angular-smart-home-ui/src/app/sidebar/sidebar.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DeviceDetectorService } from '../common/service/device-detector.service';
+import { Footer } from './footer/footer';
+import { Header } from "./header/header";
+import { Menu } from "./menu/menu";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatButtonModule, MatSidenavModule, Footer, Header, Menu],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  readonly #breakPointService = inject(DeviceDetectorService);
 
+  protected isMobile = this.#breakPointService.isMobile;
+
+  public ngOnInit() {}
 }
