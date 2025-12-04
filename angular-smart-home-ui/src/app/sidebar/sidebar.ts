e2f03f9ef164d7012a38ad1e35a5e82e50e9b381
Glib Shemenkov <shemenkovglib@gmail.com>
2025-12-04 16:18:54 +0100

    feat: end design sidebar

--- angular-smart-home-ui/src/app/sidebar/sidebar.ts
+++ angular-smart-home-ui/src/app/sidebar/sidebar.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { DeviceDetectorService } from '../common/service/device-detector.service';
 import { Footer } from './footer/footer';
-import { Header } from "./header/header";
-import { Menu } from "./menu/menu";
+import { Header } from './header/header';
+import { Menu } from './menu/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatButtonModule, MatSidenavModule, Footer, Header, Menu],
+  imports: [MatButtonModule, MatSidenavModule, MatIconModule, Footer, Header, Menu],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
